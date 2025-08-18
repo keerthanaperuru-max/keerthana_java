094dae17c2403221dad43f745ac91f45940467f4
keerthanaperuru-max <keerthanaperuru@gmail.com>
2025-08-18 10:41:01 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,27 @@
+class bubblesort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] data = {20, 5, 65, 45, 67, 8};
+        System.out.println("Before sorting");
+        for (int num : data) {
+            System.out.print(num + " ");
+        }
+        bubbleSort(data);
+        System.out.println("\nAfter sorting");
+        for (int num : data) {
+            System.out.print(num + " ");
+        }
+    }
+}
